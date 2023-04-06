9aa1e509d4ab36492ab31449ede3639554e3b4ec
nalinikantheti <nalinikantheti@gmail.com>
2023-04-06 19:17:52 -0400

    searchscreen

---
+++ src/search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../vendors/bootstrap.min.css'; 
+import TrackStarHeader from '../trackstar-header';
+
+function Search() {
+  return (
+    <>
+    <TrackStarHeader/>
+    
+    </>
+  );
+}
+
+export default Search;
