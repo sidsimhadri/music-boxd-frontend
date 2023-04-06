1e7dbea9d35ec1cb71c0894df7047dee6db21785
nalinikantheti <nalinikantheti@gmail.com>
2023-04-06 10:47:35 -0400

    profile screen sizing

--- src/profile/profile-picture.js
+++ src/profile/profile-picture.js
@@ -2,7 +2,9 @@ const ProfilePictureComponent = ({user}) => {
     return (
         <>
             <button className="profile-pic" onClick="document.getElementById('choose-profile-picture').click();">
-                <img className="profile-picture larger float-left" src={user.image} alt="" />
+                <img className="profile-picture larger float-left" alt = "" 
+                //  style={{width: '70%', height: 'auto'}}
+                 src={user.image} alt="" />
                 <input type="file" id="choose-profile-picture" />
             </button>
         </>
