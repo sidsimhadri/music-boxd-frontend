3464f5bdd2e53c225659f3d2ba383e5315cca1af
nalinikantheti <nalinikantheti@gmail.com>
2023-04-06 18:58:08 -0400

    ui

--- src/login/index.js
+++ src/login/index.js
@@ -1,8 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '../vendors/bootstrap.min.css'; 
+import TrackStarLogo from '../trackstar-header/trackstar-logo';
 
-function LoginComponent() {
-    return (<>LOGIN </>
-    );
-};
+function Login() {
+  return (
+    <>
+    <TrackStarLogo/>
+    <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '75vh' }}>
+      <div className="p-4 bg-dark rounded shadow-sm">
+        <div className="text-center mb-4">
+          <h1 className="mt-2">Login</h1>
+        </div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email" className="text-muted small">Email address</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+          </div>
+        
+          <div className="form-group mt-2">
+            <label htmlFor="password" className="text-muted small">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password" />
+          </div>
 
-export default LoginComponent;
+          <div className="form-group form-check mt-2">
+            <input type="checkbox" className="form-check-input" id="remember-me" />
+            <label className="form-check-label" htmlFor="remember-me">Remember me</label>
+          </div>
+
+
+          <button type="submit" className="btn btn-success  mt-2 btn-block">Login</button>
+        </form>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Login;
