a5cd0944b4f07f04666e2bfe095709a67d152232
nalinikantheti <nalinikantheti@gmail.com>
2023-04-11 14:19:08 -0400

    search links to search page

--- src/profile/profile-picture.js
+++ src/profile/profile-picture.js
@@ -2,7 +2,7 @@ const ProfilePictureComponent = ({user}) => {
     return (
         <>
             <button className="profile-pic" onClick="document.getElementById('choose-profile-picture').click();">
-                <img className="profile-picture larger float-left" src={user.image} alt="" />
+                <img className="profile-picture larger float-left" alt = "description" src={user.image} alt="" />
                 <input type="file" id="choose-profile-picture" />
             </button>
         </>
--- src/trackstar-header/main-search-bar.js
+++ src/trackstar-header/main-search-bar.js
@@ -1,9 +1,16 @@
-function MainSearchBar() {
-    return (
-    <span className="form-group float-left ms-3 me-4" style={{width: "60%"}}>
-        <input type="text" className="form-control" placeholder="Search" id="inputDefault"/>
-    </span>
-    );
-};
 
-export default MainSearchBar;
+function MainSearchBar() {
+    function handleKeyDown(event) {
+      if (event.keyCode === 13) {
+        window.location.href = "/search";
+      }
+    }
+  
+    return (
+      <span className="form-group float-left ms-3 me-4" style={{width: "60%"}}>
+        <input type="text" className="form-control" placeholder="Search" id="inputDefault" onKeyDown={handleKeyDown} />
+      </span>
+    );
+  };
+  
+  export default MainSearchBar;
