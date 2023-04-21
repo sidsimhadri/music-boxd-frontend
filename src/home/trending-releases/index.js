eae6a913d1f59dace2083a022cbdf3578d894f4f
Jack Furci <furci.j@northeastern.edu>
2023-04-21 18:32:11 -0400

    more changes

--- src/home/trending-releases/index.js
+++ src/home/trending-releases/index.js
@@ -1,8 +1,33 @@
-import releases from "./releases";
 import '../index.css';
 import TrendingReleaseItem from "./trending-release-item";
+import * as service from "../../services/service";
+import React, { useState, useEffect } from "react";
+
+function filterAlbums(releaseList) {
+    let filtered = []
+    releaseList.forEach((item) => {
+      if (item.album_group === "album") {
+        filtered.push(item)
+      }
+    })
+    return filtered
+}
 
 const TrendingReleases = () => {
+    const [releases, setReleases] = useState([])
+    const [promise, setPromise] = useState(null)
+    useEffect(() => {
+        setPromise(service.findNewreleases())
+    }, [])
+
+    useEffect(() => {
+        if (promise !== null) {
+            promise.then((response) => {
+                setReleases(filterAlbums(response.body.albums.items))
+            })
+        }
+    }, [promise])
+
     return (
         <>
             <div className="bg-dark text-white pt-3 ps-3 pb-1 bg-dark">
--- src/home/trending-releases/trending-release-item.js
+++ src/home/trending-releases/trending-release-item.js
@@ -1,19 +1,19 @@
 import StarRating from "../../star-rating/index.js";
 import { Link } from "react-router-dom";
 
-const TrendingReleaseItem = ({ release }) => {
+const TrendingReleaseItem = ({ release }) => { 
     return (<>
         <div className="list-group-item no-rounded-tops">
-            <img className="album-cover-trending-release" src={release.image} alt={release.title} />
-            <Link className="link-salmon" to="/"><h6 className="mb-0 volkhov">{release.title}</h6></Link>
+            <img className="album-cover-trending-release" src={release.images[0].url} alt={release.title} />
+            <Link className="link-salmon" to="/"><h6 className="mb-0 volkhov tr-nudge-left">{release.name}</h6></Link>
             <div>
-                <span><Link className="link-salmon float-left text-small nunito" to="/">{release.artist}</Link></span>
-                <span className="text-muted text-small float-left nunito ps-1">• {release.year}</span>
-            </div><br />
-            <div className="text-muted text-small nunito">
+                <div><Link className="link-salmon text-small nunito tr-nudge-left" style={{ "wordBreak": "break-word" }} to="/">{release.artists[0].name}</Link></div>
+                <span className="text-muted text-small nunito ps-1 tr-nudge-left" style={{ "wordBreak": "keep-all" }}>• {release.release_date}</span>
+            </div>
+            <div className="text-muted text-small nunito tr-nudge-left">
                 Reviews: {release.reviews}
             </div>
-            <StarRating rating={release.averageRating} />
+            {/* <StarRating rating={release.averageRating} /> */}
         </div></>
     );
 }
