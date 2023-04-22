95b22954183f598cfd5e63b6c074a9b6988a0732
Jack Furci <furci.j@northeastern.edu>
2023-04-22 13:04:41 -0400

    It work now

--- src/trackstar-header/profile-information.js
+++ src/trackstar-header/profile-information.js
@@ -5,28 +5,51 @@ import { profileThunk } from "../services/auth-thunks";
 
 
 function ProfileInformation() {
-    const currentUser = useSelector((state) => 
+    const currentUser = useSelector((state) =>
         state.auth.currentUser
     );
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(profileThunk());
-      }, []);
-     
+    }, []);
+
+    const [profile, setProfile] = useState({
+        "username": "",
+    })
+
     const [profileName, setProfileName] = useState("")
     useEffect(() => {
         if (currentUser !== null && currentUser !== undefined) {
-            setProfileName(currentUser.currentUser.username)
+            setProfile(currentUser.currentUser)
         }
-    },[currentUser])
-    return (
-        <div className="d-flex center">
-            <Link className="link-salmon d-flex center" to="/profile">
-                <h6 className="nunito float-right mt-2">@{profileName}</h6>
-                <img className="profile-picture float-right me-3 ms-3" src="../../images/benson.jpeg" alt=""/>       
-            </Link>
-        </div>
+    }, [currentUser])
+
+    useEffect(() => {
+        if (profile !== undefined) {
+            if (profile.username !== undefined && profile.username !== null) {
+                setProfileName(profile.username)
+            }
+        }
+    }, [profile])
+
+    return (<>
+        {currentUser &&
+            <div className="d-flex center">
+                <Link className="link-salmon d-flex center" to="/profile">
+                    <h6 className="nunito float-right mt-2">@{profileName}</h6>
+                    <img className="profile-picture float-right me-3 ms-3" src="../../images/benson.jpeg" alt="" />
+                </Link>
+            </div>
+        }
+        {!currentUser &&
+            <button className="btn btn-success me-2 btn-block nunito">
+                <Link className="text-white" to="/login">
+                    Login
+                </Link>
+            </button>
+        }
+    </>
     );
 };
 
