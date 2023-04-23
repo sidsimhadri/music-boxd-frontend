b78fa9d339700955ae82bfe110d06b800b37707b
Priyanka Padinam <padinampriya@gmail.com>
2023-04-23 18:03:50 -0400

    edited artists page

--- src/artists/index.js
+++ src/artists/index.js
@@ -24,6 +24,7 @@ function ArtistAlbumsComponent() {
     const [artistAlbums, setArtistAlbums] = useState(null)
     const [albumItems, setAlbumItems] = useState([])
     const [promise, setPromise] = useState(null)
+    const [artistName, setArtistName] = useState("")
     useEffect(() => {
         setPromise(findArtistAlbums(artistId))
     }, [artistId])
@@ -38,30 +39,31 @@ function ArtistAlbumsComponent() {
       if (artistAlbums !== null) {
         setAlbumItems(filterDuplicateAlbums(artistAlbums.items))
         setArtistLoading(false)
+        setArtistName(artistAlbums.items[0].artists[0].name)
+      // console.log()
       }
     }, [artistAlbums])
+
     return (<>
     <div className="row mt-2">
                 <TrackStarHeader />
             </div>
     {!artistLoading && (
       <div className="row mt-2">
+      <h1 className="row mt-2 nunito-font font-weight-12"><h1 className="col-1">Artist: </h1> <h1 className="ms-4 col-10">{artistName}</h1></h1>
         {albumItems.map((album) => (
           <div className="card border-dark mb-3 nunito" style={{ width: "80%" }}>
             <div className="row card-body">
              <div className="col-10">
               <h4 className="row">
-              <div className="col-3">Album:</div>
-                <div className="col-9">
-                  <Link className="text-white" to={`/albums/${album.id}`} style={{ textDecoration: "none" }}>{album.name}</Link>
+              <div className="col-4">Album:</div>
+                <div className="col-8">
+                  <Link to={`/albums/${album.id}`} style={{ textDecoration: "none" }}>{album.name}</Link>
                 </div>
               </h4>
-              <h5 className="row">
-              <div className="col-3">Artist:</div>
-               <div className="col-9"> <Link to={`/artists/${album.artists[0].id}`} style={{ textDecoration: "none" }}>{album.artists[0].name}</Link></div>
-              </h5>
+
               <div className="row">
-              <h5 className="col-3">Release date:</h5> <div className="col-9 mt-1 salmon-text"><h6>{album.release_date}</h6></div>
+              <h4 className="col-4">Release date:</h4> <div className="col-8 mt-1 salmon-text"><h5>{album.release_date}</h5></div>
               </div>
              </div>
              <div className="col-2">
