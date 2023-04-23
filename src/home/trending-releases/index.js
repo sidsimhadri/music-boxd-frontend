4f3d308a2526f9f3028bbcd64cdad6e6cffe76a2
Jack Furci <furci.j@northeastern.edu>
2023-04-23 14:08:45 -0400

    only commit here

--- src/home/trending-releases/index.js
+++ src/home/trending-releases/index.js
@@ -1,12 +1,55 @@
-import releases from "./releases";
 import '../index.css';
 import TrendingReleaseItem from "./trending-release-item";
+import * as service from "../../services/service";
+import React, { useState, useEffect } from "react";
+
+function filterDuplicateAlbums(albumList) {
+    let filtered = []
+    let names = []
+    albumList.forEach((item) => {
+      if (!names.includes(item.name)) {
+        names.push(item.name)
+        filtered.push(item)
+      }
+    })
+    return filtered
+  }
+
+function filterAlbums(releaseList) {
+    let albums = []
+    let not_albums = []
+    releaseList.forEach((item) => {
+      if (item.album_group === "album") {
+        albums.push(item)
+      } else {
+        not_albums.push(item)
+      }
+      if (albums.length < 5) {
+        albums = albums.concat(not_albums.slice(0, 5 - albums.length - 1))
+      }
+    })
+    return filterDuplicateAlbums(albums)
+}
 
 const TrendingReleases = () => {
+    const [releases, setReleases] = useState([])
+    const [promise, setPromise] = useState(null)
+    useEffect(() => {
+        setPromise(service.findNewreleases())
+    }, [])
+
+    useEffect(() => {
+        if (promise !== null) {
+            promise.then((response) => {
+                setReleases(filterAlbums(response.body.albums.items))
+            })
+        }
+    }, [promise])
+
     return (
         <>
-            <div className="bg-dark text-white pt-3 ps-3 pb-1 bg-dark rounded">
-                <h6 className="nunito">Trending Releases</h6>
+            <div className="bg-dark text-white pt-3 ps-3 pb-1 bg-dark">
+                <h6 className="nunito">New Releases</h6>
             </div>
             <ul className="list-group">
                 {
